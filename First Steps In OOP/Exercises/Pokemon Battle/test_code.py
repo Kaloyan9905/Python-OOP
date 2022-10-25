2131c977f84ec286fe4f206f0de7e3a81dd343ca
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-10-25 20:28:59 +0300

    Add files via upload

---
+++ First Steps In OOP/Exercises/Pokemon Battle/test_code.py
@@ -0,0 +1,13 @@
+from first_steps_in_oop_exercises.pokemon_battle.pokemon import Pokemon
+from first_steps_in_oop_exercises.pokemon_battle.trainer import Trainer
+
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
