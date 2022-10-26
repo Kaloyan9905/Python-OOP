e249017515fef96dda36eb65226dbf7bf86281bf
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-10-26 20:00:45 +0300

    Add files via upload

---
+++ Classes And Objects/Lab/account.py
@@ -0,0 +1,25 @@
+class Account:
+
+    def __init__(self, id: int, name: str, balance: int = 0):
+        self.id = id
+        self.name = name
+        self.balance = balance
+
+    def credit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def debit(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            return self.balance
+        return "Amount exceeded balance"
+
+    def info(self):
+        return f"User {self.name} with account {self.id} has {self.balance} balance"
+
+
+account = Account(1234, "George", 1000)
+print(account.credit(500))
+print(account.debit(1500))
+print(account.info())
