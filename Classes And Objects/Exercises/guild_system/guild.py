603b935c1600235d649ecabeb5e3d8031d2460a5
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-11-02 14:53:55 +0200

    Add files via upload

---
+++ Classes And Objects/Exercises/guild_system/guild.py
@@ -0,0 +1,36 @@
+from guild_system.player import Player
+
+
+class Guild:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player: Player):
+        if player.guild == self.name:
+            return f"Player {player.name} is already in the guild."
+
+        if player.guild != player.DEFAULT_GUILD_NAME:
+            return f"Player {player.name} is in another guild."
+
+        self.players.append(player)
+        player.guild = self.name
+        return f"Welcome player {player.name} to the guild {player.guild}"
+
+    def kick_player(self, player_name: str):
+        for player in self.players:
+            if player.name == player_name:
+                self.players.remove(player)
+                player.guild = Player.DEFAULT_GUILD_NAME
+                return f"Player {player_name} has been removed from the guild."
+        return f"Player {player_name} is not in the guild."
+
+    def guild_info(self):
+        result = f"Guild: {self.name}\n"
+
+        for player in self.players:
+            result += player.player_info() + "\n"
+
+        return result
+
+
