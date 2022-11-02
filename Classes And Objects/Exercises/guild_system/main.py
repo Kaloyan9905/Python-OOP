a85a42160883c88ef751a7bbdbadabb8d9a67e1a
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-11-02 14:55:19 +0200

    Add files via upload

---
+++ Classes And Objects/Exercises/guild_system/main.py
@@ -0,0 +1,8 @@
+from guild_system import Player, Guild
+
+player = Player("George", 50, 100)
+print(player.add_skill("Shield Break", 20))
+print(player.player_info())
+guild = Guild("UGT")
+print(guild.assign_player(player))
+print(guild.guild_info())
