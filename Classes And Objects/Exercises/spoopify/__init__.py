a1375abd8b8a0f75aee065aa3c1d422dedba4ebe
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-11-02 14:56:27 +0200

    Update __init__.py

--- Classes And Objects/Exercises/spoopify/__init__.py
+++ Classes And Objects/Exercises/spoopify/__init__.py
@@ -1,15 +1,1 @@
-from classes_and_objects_exercises.spoopify.album import Album
-from classes_and_objects_exercises.spoopify.band import Band
-from classes_and_objects_exercises.spoopify.song import Song
 
-song = Song("Running in the 90s", 3.45, False)
-print(song.get_info())
-album = Album("Initial D", song)
-second_song = Song("Around the World", 2.34, False)
-print(album.add_song(second_song))
-print(album.details())
-print(album.publish())
-band = Band("Manuel")
-print(band.add_album(album))
-print(band.remove_album("Initial D"))
-print(band.details())
