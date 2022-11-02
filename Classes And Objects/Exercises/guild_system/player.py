c1e1c151b7a32ab69186c095b7a7297372562a58
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-11-02 14:54:48 +0200

    Add files via upload

---
+++ Classes And Objects/Exercises/guild_system/player.py
@@ -0,0 +1,29 @@
+class Player:
+    DEFAULT_GUILD_NAME = "Unaffiliated"
+
+    def __init__(self, name, hp, mp):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = Player.DEFAULT_GUILD_NAME
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return "Skill already added"
+
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        result = f"Name: {self.name}\n"
+        result += f"Guild: {self.guild}\n"
+        result += f"HP: {self.hp}\n"
+        result += f"MP: {self.mp}\n"
+
+        for skill_name, mana_cost in self.skills.items():
+            result += f"==={skill_name} - {mana_cost}\n"
+
+        return result
+
+
