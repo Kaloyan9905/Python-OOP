a072bd3cb804d4d26210ff4267a04b791f7173b6
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-11-29 11:44:59 +0200

    Add files via upload

---
+++ Inheritance/Exercises/players_and_monsters/soul_master.py
@@ -0,0 +1,6 @@
+from players_and_monsters.dark_wizard import DarkWizard
+
+
+class SoulMaster(DarkWizard):
+    def __init__(self, username: str, level: int):
+        super().__init__(username, level)
