0b7a7aa2daf9bdc5dfdce4b3d528de6fd4657583
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-12-26 20:15:34 +0200

    Add files via upload

---
+++ Static and Class Methods/Exercises/movie_world/movie_world.py
@@ -0,0 +1,72 @@
+from movie_world.customer import Customer
+from movie_world.dvd import DVD
+
+
+class MovieWorld:
+    def __init__(self, name):
+        self.name = name
+        self.customers = []
+        self.dvds = []
+
+    @staticmethod
+    def dvd_capacity():
+        return 15
+
+    @staticmethod
+    def customer_capacity():
+        return 10
+
+    def add_customer(self, customer: Customer):
+        if len(self.customers) < self.customer_capacity():
+            self.customers.append(customer)
+
+    def add_dvd(self, dvd: DVD):
+        if len(self.dvds) < self.dvd_capacity():
+            self.dvds.append(dvd)
+
+    def rent_dvd(self, customer_id: int, dvd_id: int):
+        customer = self.__find_customer_by_id(customer_id)
+        dvd = self.__find_dvd_by_id(dvd_id)
+
+        if dvd in customer.rented_dvds:
+            return f"{customer.name} has already rented {dvd.name}"
+
+        if dvd.is_rented:
+            return "DVD is already rented"
+
+        if customer.age < dvd.age_restriction:
+            return f"{customer.name} should be at least {dvd.age_restriction} to rent this movie"
+
+        customer.rented_dvds.append(dvd)
+        dvd.is_rented = True
+        return f"{customer.name} has successfully rented {dvd.name}"
+
+    def return_dvd(self, customer_id, dvd_id):
+        customer = self.__find_customer_by_id(customer_id)
+        dvd = self.__find_dvd_by_id(dvd_id)
+
+        if dvd in customer.rented_dvds:
+            customer.rented_dvds.remove(dvd)
+            dvd.is_rented = False
+            return f"{customer.name} has successfully returned {dvd.name}"
+        return f"{customer.name} does not have that DVD"
+
+    def __repr__(self):
+        result = ""
+        for customer in self.customers:
+            result += repr(customer) + "\n"
+
+        for dvd in self.dvds:
+            result += repr(dvd) + "\n"
+
+        return result
+
+    def __find_customer_by_id(self, customer_id):
+        for customer in self.customers:
+            if customer.id == customer_id:
+                return customer
+
+    def __find_dvd_by_id(self, dvd_id):
+        for dvd in self.dvds:
+            if dvd.id == dvd_id:
+                return dvd
