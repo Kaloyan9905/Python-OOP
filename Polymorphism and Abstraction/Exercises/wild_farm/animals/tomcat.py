fc30aaaa8b1ff3807c4df2e8ee80760237e71c10
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2022-12-28 22:59:36 +0200

    Delete tomcat.py

--- Polymorphism and Abstraction/Exercises/wild_farm/animals/tomcat.py
+++
@@ -1,9 +0,0 @@
-from animals.cat import Cat
-
-
-class Tomcat(Cat):
-    def __init__(self, name, age):
-        super().__init__(name, age, "Male")
-
-    def make_sound(self):
-        return "Hiss"
