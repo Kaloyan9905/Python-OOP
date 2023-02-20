890e731d9a225b6ff614d5e09de0d5aefb986c4e
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2023-02-20 17:12:25 +0200

    Add files via upload

---
+++ Iterators and Generators/Lab/custom_range.py
@@ -0,0 +1,20 @@
+class custom_range:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start <= self.end:
+            to_be_returned = self.start
+            self.start += 1
+            return to_be_returned
+        else:
+            raise StopIteration()
+
+
+one_to_ten = custom_range(1, 10)
+for num in one_to_ten:
+    print(num)
