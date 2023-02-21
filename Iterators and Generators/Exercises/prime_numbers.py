be58df17b7526611cc49e142af3a7362a1c8221f
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2023-02-21 18:19:02 +0200

    Add files via upload

---
+++ Iterators and Generators/Exercises/prime_numbers.py
@@ -0,0 +1,15 @@
+def get_primes(numbers):
+    def is_prime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    for num in numbers:
+        if is_prime(num):
+            yield num
+
+
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
